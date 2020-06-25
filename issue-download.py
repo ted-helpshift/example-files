19e6a69a566926c010e2ed2787ecc20d1345e9df
Ted Stetzel (Helpshift) <52221442+ted-helpshift@users.noreply.github.com>
2020-06-25 00:33:46 -0700

    update to use last ID

--- issue-download.py
+++ issue-download.py
@@ -1,69 +1,63 @@
 import requests
 import time
+import datetime
 import json
 from base64 import b64encode
 
-# Step 1: Enter your start and end date here
-# You can get the data in epoch time here: https://www.epochconverter.com/
-start_date = 1569801600000 # sept 2019
-end_date = 1593056460000 #june 25 2020
 
-#Constants - Don't change
-DAY = 86400000
-WEEK = 604800000
+def get_timestamp_in_ms(date):
+   return str(int(time.mktime(datetime.datetime.strptime(date, "%m/%d/%Y").timetuple()) * 1e3))
 
-#Step 2 : pick your increment day or week
-increment = WEEK
-increment_name = "Week"
+#  Suply your start and end date Key from 
+start_date = "09/11/2019"
+end_date = "12/11/2019"
 
-# Step 3 Suply your API Key from 
+#  Suply your API Key from 
 DOMAIN = "YOUR DOMAIN"
 HELPSHIFT_API_KEY = "YOUR KEY"
 
-# Don't change this - Convert API Key from Helpshift to Base 64
+
+#Converting your Variables - Don't Change 
+start_ts = get_timestamp_in_ms(start_date)
+end_ts = get_timestamp_in_ms(end_date)
 encoded_credentials = b64encode(bytes(f'{HELPSHIFT_API_KEY}:""',
                                 encoding='ascii')).decode('ascii')
 
 #CREATE A CSV FILE TO WRITE IDs 
 f = open("ID-LIST.csv", "w")
 
-#LOOP CODE 
-while start_date < end_date:
-    pages = 1
-    i = 1
-    readable_startdate = time.strftime("%Z - %Y/%m/%d, %H:%M:%S", time.localtime(start_date/1000))
+while 1:
+
+    headers = {
+        'authorization': 'Basic {}'.format(encoded_credentials),
+        }
+    r = requests.get(
+        ('https://api.helpshift.com/v1/'+ DOMAIN +'/issues?sort-by=creation-time&sort-order=asc&created_since='
+        + start_ts + '&created_until=' + end_ts + '&page-size=1000'),
+        headers= headers )
+    data = r.json()
+    print(r.text[0:100],"...")
+
     
-    while i <= pages:
-        url = "https://api.helpshift.com/v1/%s/issues"%(DOMAIN)
+    # All the issues for the given query
+    issues = (data['issues'])
+    
+    if not issues:
+        print("No issues. Done")
+        break
 
-        querystring = {
-        # "languages":"{\"and\" : [\"en\"] }",
-            "page-size":1000,
-            "created_since": start_date,
-            "created_until":  start_date + increment,
-            "page":"%s"%(i)}
+    x = json.loads(r.text)
+    
+    #Write each ID to a CSV
+    for item in x["issues"]:
+        index = x["issues"].index(item)
+        length = len(x["issues"]) - 1
+        if str(item["redacted"]) == "False" and index != length:
+            f.write("%s\n"%(item["id"]))
+            
+        if str(item["redacted"]) == "True" and index != length:
+            print("Issue ID %s was redacted"%(item["id"]))
+            f.write("%s\n"%(item["id"]))
 
-        headers = {
-            'authorization': 'Basic {}'.format(encoded_credentials),
-            }
-
-        response = requests.request("GET", url, headers=headers, params=querystring)
-        print(response.text[0:100],"...")
-        
-
-        #GO THROUGH EACH ISSUE AND DO SOMETHING: 
-        x = json.loads(response.text)
-        pages = x["total-pages"]
-
-        for item in x["issues"]:
-            if str(item["redacted"]) == "False":
-                f.write("%s\n"%(item["id"]))
-            else:
-                print("Issue ID %s was redacted"%(item["id"]))
-                f.write("%s\n"%(item["id"]))
-        print("----------- For %s Starting %s Write page %s of %s -----------"%(increment_name, readable_startdate, i, x["total-pages"]))
-        time.sleep(.5)
-        i += 1
-    start_date += increment
-
-print("------------------ Download Complete ------------------")
+    start_ts = str(issues[-1]['created_at'])
+    time.sleep(.75)
