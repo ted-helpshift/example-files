cf3bcbd1493d193bba45f56b93d0aca3e2a45e89
Ted Stetzel (Helpshift) <52221442+ted-helpshift@users.noreply.github.com>
2020-06-26 20:10:02 -0700

    End loop

--- issue-download.py
+++ issue-download.py
@@ -9,12 +9,12 @@ def get_timestamp_in_ms(date):
    return str(int(time.mktime(datetime.datetime.strptime(date, "%m/%d/%Y").timetuple()) * 1e3))
 
 #  Suply your start and end date Key from 
-start_date = "09/11/2019"
-end_date = "12/11/2019"
+start_date = "06/20/2020"
+end_date = "06/22/2020"
 
 #  Suply your API Key from 
 DOMAIN = "YOUR DOMAIN"
-HELPSHIFT_API_KEY = "YOUR KEY"
+HELPSHIFT_API_KEY = "YOUR API KEY"
 
 
 #Converting your Variables - Don't Change 
@@ -36,7 +36,7 @@ while 1:
         + start_ts + '&created_until=' + end_ts + '&page-size=1000'),
         headers= headers )
     data = r.json()
-    print(r.text[0:100],"...")
+    print(r.text[0:200],"...")
 
     
     # All the issues for the given query
@@ -59,5 +59,18 @@ while 1:
             print("Issue ID %s was redacted"%(item["id"]))
             f.write("%s\n"%(item["id"]))
 
+    if len(issues) == 1:
+        # This is the last issue process it.
+        issues_to_be_processed = issues
+    else:
+        # Drop the last issue and process it in the next loop because created-since of last issue is taken and it will be
+        # the first issue in the next query.
+        issues_to_be_processed = issues[:-1]
+    # issues_to_be_processed is the array which you should process it further like write to your external system etc.
+
+    if len(issues) == 1:
+        print('This is the only 1 issue in the chunk. Process it and you are done.')
+        break
+
     start_ts = str(issues[-1]['created_at'])
-    time.sleep(1.5)
+    time.sleep(.75)
